463d750983df72af3f017482cdb446ae1e80fe0f
cortinasnaydu <cortinas.naydu@gmail.com>
2026-05-17 16:24:32 -0500

    Add language selection functionality js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function setLanguage(lang) {
+
+  document.querySelectorAll('.lang-es').forEach(el => {
+    el.style.display = lang === 'es' ? '' : 'none';
+  });
+
+  document.querySelectorAll('.lang-en').forEach(el => {
+    el.style.display = lang === 'en' ? '' : 'none';
+  });
+
+  localStorage.setItem('language', lang);
+}
+
+const savedLanguage =
+  localStorage.getItem('language') || 'es';
+
+setLanguage(savedLanguage);
