b0ec5981c9d3b7f932d29fdae1f85f42cb045e3e
danfitzpatrick <dan.fpatrick@gmail.com>
2026-05-30 15:51:14 -0500

    Add the slider code

--- js/script.js
+++ js/script.js
@@ -19,3 +19,28 @@ setLanguage(savedLanguage);
 document.querySelectorAll(".year").forEach((el) => {
   el.textContent = new Date().getFullYear();
 });
+
+const photoSlider = new Swiper(".photo-slider", {
+  loop: true,
+  spaceBetween: 18,
+  speed: 1200,
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
